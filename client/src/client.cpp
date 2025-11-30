70782d08a81b528aa2731aa4f7021aeb3c9fbb60
JManion32 <jmanion32@gmail.com>
2025-11-30 12:03:00 -0500

    Add protocol

--- client/src/client.cpp
+++ client/src/client.cpp
@@ -1,16 +1,15 @@
-#include "../include/client.h"
+#include "../include/client.hpp"
+#include "../include/protocol_qt.hpp"
 #include <QMessageBox>
-#include <QDebug>
 
 Client::Client(const QString &ip, quint16 port, QWidget *parent)
-    : QMainWindow(parent), socket(new QTcpSocket(this))
+    : QMainWindow(parent),
+      socket(new QTcpSocket(this))
 {
     socket->connectToHost(ip, port);
 
-    if (socket->waitForConnected(5000)) {
-        qDebug() << "Connected!";
-    } else {
-        qDebug() << "Connection failed:" << socket->errorString();
+    if (!socket->waitForConnected(3000)) {
+        QMessageBox::critical(this, "Connection Error",
+                              "Failed to connect to server.");
     }
 }
-
