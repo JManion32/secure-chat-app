8facf55c80e09576613ad373948901a2b04228e6
JManion32 <jmanion32@gmail.com>
2026-01-08 19:54:14 -0500

    Fix npm run build

--- client_site/src/pages/Shop.tsx
+++ client_site/src/pages/Shop.tsx
@@ -1,5 +1,5 @@
 //import { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+//import { useNavigate } from 'react-router-dom';
 import ShopPanel from '../components/ShopPanel.tsx'
 
 import '../css/shop.css';
